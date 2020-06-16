500554d022127b70e7b1e1432db4ba373d29ac06
BartTheScrivner <dframe22@gmail.com>
2020-06-16 12:57:10 -0400

    added average_rating & most_popular_overall to Podcast

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -2,4 +2,20 @@ class Podcast < ActiveRecord::Base
   has_many :subscriptions
   has_many :users, through: :subscriptions
 
+  
+
+  def average_rating
+    ratings = self.subscriptions.map {|sub| sub.rating}
+    if ratings.compact.count == 0
+      puts "#{self.name} hasn't been rated yet."
+      return 0
+    else
+      ratings.compact.sum/ratings.compact.count
+    end
+  end
+
+  def self.most_popular_overall
+    puts self.all.group_by{|podcast| podcast.average_rating}.max
+  end
+
 end
