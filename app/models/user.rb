1c096b0f04eff46aa97be396274b53b10b2bfe1e
BartTheScrivner <dframe22@gmail.com>
2020-06-16 12:07:18 -0400

    made search_podcasts and subscribe_by_name in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
 class User < ActiveRecord::Base 
  has_many :subscriptions
  has_many :podcasts, through: :subscriptions
+
+ def search_podcasts(podcast_name)
+  podcast = Podcast.find_by(name: podcast_name)
+  if podcast == nil
+    puts "Sorry we can't find that one."
+  else
+    podcast
+  end
+ end
+
+ def subscribe_by_name(podcast_name)
+  podcast = search_podcasts(podcast_name)
+  if !podcast.nil?
+    Subscription.create(user_id: self.id, podcast_id: podcast.id)
+  end
+ end
+
+
 end
