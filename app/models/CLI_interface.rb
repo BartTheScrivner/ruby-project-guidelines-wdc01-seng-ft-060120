b7e4278e7d4fceaec161005589dc6dac27a2d013
BartTheScrivner <dframe22@gmail.com>
2020-06-17 11:44:25 -0400

    changed CLI to OO, added current_user & welcome

---
+++ app/models/CLI_interface.rb
@@ -0,0 +1,74 @@
+class Interface 
+  attr_accessor :input, :current_user
+  def welcome
+  puts "*" * 20
+  puts "Welcome to Pod-if-i"
+  self.get_user
+  self.menu_options
+  end
+
+  def goodbye
+    puts "GOODBYE!"
+  end
+
+  def get_user
+    puts "Enter user name: "
+    user = gets.chomp()
+    self.current_user = User.find_or_create_by(name: user)
+  end
+
+  def custom_welcome
+    puts "Welcome, #{self.current_user.name}!"
+  end
+
+
+  def menu_options
+    custom_welcome
+    puts "Please select from the following or type 'quit' to exit:"
+    puts "1. My Subscriptions -- type 'subs' "
+    puts "2. My Friends -- type 'friends' "
+    puts "3. Find Podcasts -- 'find' "
+    self.input = self.get_menu_selection
+  end
+
+  def get_menu_selection
+    selection = gets.chomp()
+    selection.downcase 
+  end
+
+  def friends_menu
+    puts "Welcome to the Friends Menu. Select from the following: "
+    puts "1. Select Friend -- type 'select' "
+    puts "2. See All Friends -- type 'all' "
+    puts "3. See All Friends' Subscriptions -- type 'allsubs' "
+  end
+
+  def find_menu 
+    puts "Welcome to the Find Menu. Select from the following: "
+    puts "1. Search Podcasts by Name -- type 'name' "
+    puts "2. Search Podcasts by Length -- type 'length' "
+    puts "3. See Most Popular Podcasts -- type 'popular' "
+  end
+
+  def run
+    self.welcome
+    # binding.pry
+    while self.input != 'quit'
+      if self.input == 'subs'
+        puts 'selected subs menu'
+        menu_options
+      elsif self.input == 'friends'
+        puts 'selected friends menu'
+        menu_options
+      elsif self.input == 'find'
+        puts 'selected find menu'
+        menu_options
+      else
+        puts "Improper selection. Please try again."
+        menu_options
+      end
+    end 
+    self.goodbye
+  end
+
+end
--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,20 @@
 require_relative '../config/environment'
+interface = Interface.new
+
+interface.run
 
 
+# input = interface.get_menu_selection
 
-puts "HELLO WORLD"
+# case input
+#   binding.pry
+#   when 'subs'
+#     current_user.podcasts
+#   when 'friends'
+#     friends_menu
+#   when 'find'
+#     find_menu
+#   else
+#     puts "Improper selection, please try again."
+#     menu_options
+# end
